82881b60cfe3d638cd7372b7ed6d8d69b1e29278
robin ELZEARD <robinelzeard@gmail.com>
2019-03-04 17:01:51 +0100

    (a terminer) navigation

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Search from './Components/Search'
-import {View} from 'react-native'
+import Navigation from './Navigation/Navigation'
 
 export default class App extends React.Component {
   render() {
     return (
-		<View  style={{ flex: 1 }}>
-			<Search/>
-		</View>
+		<Navigation/>
     );
   }
 }
