b4ae6f5dde06c5bf26cc4a95659b69aed01778eb
robin ELZEARD <robinelzeard@gmail.com>
2019-03-04 14:46:18 +0100

    ajout de plusieurs fonctionnalitées (image, loader,

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,9 +1,15 @@
 const API_TOKEN = "249f45b1636a8156fa9fc3803fd77a82"
 
-export function getFilmFromApiWithSearchedText(text) {
+export function getFilmFromApiWithSearchedText(text) 
+{
 	const url = 'https://api.themoviedb.org/3/search/movie?api_key='+ API_TOKEN + '&language=fr&query=' + text
 	return fetch(url)
 		.then((response) => response.json())
 		.catch((error) => console.log(error))
 		
+}
+
+export function getImageFromApi (name) 
+{
+	return 'https://image.tmdb.org/t/p/w300'+ name
 }
--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator} from 'react-native'
 import films from '../Helpers/data'
 import FilmItem from './FilmItem'
 import { getFilmFromApiWithSearchedText } from '../API/TMDBApi'
@@ -10,16 +10,22 @@ class Search extends React.Component
 	{
 		super(props)
 		this.state = {
-		films:[]
+			films:[],
+			isLoading: false
 		}
 		this.searchedText = ""
 	}
 	
 	_loadFilms()
 	{
+		this.setState({isLoading: true})
 		if (this.searchedText.length > 0)
 		{
-			getFilmFromApiWithSearchedText(this.searchedText).then(data => this.setState({films: data.results}))
+			getFilmFromApiWithSearchedText(this.searchedText).then(data => this.setState(
+			{
+			films: data.results,
+			isLoading: false
+			}))
 		}
 	}
 	
@@ -28,35 +34,65 @@ class Search extends React.Component
 		this.searchedText = text
 	}
 	
+	_displayLoading()
+	{
+		if (this.state.isLoading)
+		{
+			return(
+				<View style={styles.loading_container}>
+					<ActivityIndicator size="large"/>
+				</View>
+			)
+		}
+	}
+	
 	render()
 	{
-		console.log("RENDER");
+		console.log(this.state.isLoading);
 		return (
 			<View style={styles.main_container}>
-				<TextInput onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="recherche"/>
+				<TextInput onSubmitEditing={() => this._loadFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="recherche"/>
 				<Button title="rechercher" onPress={()=>this._loadFilms()}/>
 				<FlatList
 					data={this.state.films}
 					keyExtractor={(item) => item.id.toString()}
+					onEndReachedThreshold={0.5}
+					onEndReached={() => {
+						console.log("onEndReached")
+					}}
 					renderItem={({item}) => <FilmItem film={item}/>}
 				/>
+				{this._displayLoading()}
+				
 			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-	main_container: {
+	main_container: 
+	{
 		flex:1,
 		marginTop:25
 	},
-	textinput: {
+	textinput: 
+	{
 		marginLeft:5, 
 		marginRight: 5, 
 		height:50, 
 		borderColor: '#000000', 
 		borderWidth:1, 
 		paddingLeft:5
+	},
+	loading_container:
+	{
+		position: "absolute",
+		left: 0,
+		right: 0,
+		top: 100,
+		bottom: 0,
+		alignItems: 'center',
+		justifyContent: "center"
 	}
 })
 
